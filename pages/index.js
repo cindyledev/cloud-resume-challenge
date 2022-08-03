5753c3c7cb56ec6e95741b46ea88d9aadb31e8ed
Cindy Le <cindyledev@gmail.com>
2022-08-03 11:48:28 -0400

    updated build script

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="bg-white">
       <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Cindy's Cloud Resume Challenge</span>
+          <span className="block">Cindy&apos;s Cloud Resume Challenge</span>
           <span className="block">Under Construction</span>
         </h2>
         <div className="mt-8 flex justify-center">
